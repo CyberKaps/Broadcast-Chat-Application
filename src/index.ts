2a6fd64a94cd8ca80bcb587dfbc1be17135bb881
Kalpesh Borde <bordekalpesh@gmail.com>
2025-01-04 00:05:31 +0530

    server is able to send message to all the connected clients

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
+
     userCount = userCount + 1;
     console.log("user connected #" + userCount);
 
     socket.on("message", (e) => {
         console.log("message received " + e.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(e.toString() + ": sent from the server")
+        }
+        
 
-        setTimeout(() => {
-            socket.send(e.toString() + ": sent from the server")
-        },1000);
         
     })
 
